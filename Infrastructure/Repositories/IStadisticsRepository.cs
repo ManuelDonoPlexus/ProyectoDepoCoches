13c39374cc76c1bc9d4e11e379ca5efffbab52cf
ManuelDonoPlexus <manuel.donoaraujo@plexus.tech>
2026-05-25 08:32:55 +0200

    Repositorio modificado

--- Infrastructure/Repositories/IStadisticsRepository.cs
+++ Infrastructure/Repositories/IStadisticsRepository.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using CarDepo.API.Models;
+using CarDepo.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarDepo.Infrastructure.Repositories;
 
@@ -15,52 +17,73 @@ public interface IStadisticsRepository
 
 public class StadisticsRepository : IStadisticsRepository
 {
-    private readonly ICarRepository _carrepo;
-    private readonly IMakeRepository _makerepo;
-    private readonly IDriverRepository _driverrepo;
-    private readonly ICarDriverRepository _cardriverrepo;
+    private readonly CarDepoContext _cardepocontext;
 
-    public StadisticsRepository(ICarRepository carrepo, IMakeRepository makerepo, IDriverRepository driverepo, ICarDriverRepository cardriverrepo)
+    public StadisticsRepository(CarDepoContext cdc)
     {
-        _carrepo = carrepo;
-        _makerepo = makerepo;
-        _driverrepo = driverepo;
-        _cardriverrepo = cardriverrepo;
+        _cardepocontext = cdc;
     }
 
     public async Task<Decimal> GetAveragePrice()
     {
-        IEnumerable<Make> makes = await _makerepo.GetMakes();
+        IEnumerable<Make> makes = await _cardepocontext.Makes
+                .Include(m => m.FuelType)
+                .AsNoTracking()
+                .ToListAsync();
         return makes.Average(m => m.Price);
     }
 
     public async Task<Double> GetCarKms()
     {
-        IEnumerable<Car> cars = await _carrepo.GetCars();
+        IEnumerable<Car> cars = await _cardepocontext.Cars
+            .Include(car => car.Color)
+            .Include(car => car.Make)
+            .Include(car => car.Owner)
+            .AsNoTracking()
+            .ToListAsync();
         return cars.Average(c => c.Kms);
     }
 
     public async Task<IEnumerable> GetCarColorCount()
     {
-        IEnumerable<Car> cars = await _carrepo.GetCars();
+        IEnumerable<Car> cars = await _cardepocontext.Cars
+            .Include(car => car.Color)
+            .Include(car => car.Make)
+            .Include(car => car.Owner)
+            .AsNoTracking()
+            .ToListAsync(); ;
         return cars.CountBy(c => c.ColorId);
     }
 
     public async Task<IEnumerable> GetCarDriverAssociatedCarCount()
     {
-        IEnumerable<CarDriver> drivers = await _cardriverrepo.GetCarDrivers();
+        IEnumerable<CarDriver> drivers = await _cardepocontext.CarDrivers
+            .Include(cd => cd.Driver)
+            .Include(cd => cd.Car)
+            .AsNoTracking()
+            .ToListAsync();
         return drivers.CountBy(d => d.CarCDId);
     }
 
     public async Task<IEnumerable> GetCarMakeCount()
     {
-        IEnumerable<Car> cars = await _carrepo.GetCars();
+        IEnumerable<Car> cars = await _cardepocontext.Cars
+            .Include(car => car.Color)
+            .Include(car => car.Make)
+            .Include(car => car.Owner)
+            .AsNoTracking()
+            .ToListAsync(); ;
         return cars.CountBy(c => c.MakeId);
     }
 
     public async Task<IEnumerable> GetCarOwnerCount()
     {
-        IEnumerable<Car> cars = await _carrepo.GetCars();
+        IEnumerable<Car> cars = await _cardepocontext.Cars
+            .Include(car => car.Color)
+            .Include(car => car.Make)
+            .Include(car => car.Owner)
+            .AsNoTracking()
+            .ToListAsync(); ;
         return cars.CountBy(c => c.OwnerId);
     }
 }
